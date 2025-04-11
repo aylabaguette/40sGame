746367b75652ad43d33dd31ce895e814bf292c6d
aylabaguette <114880593+aylabaguette@users.noreply.github.com>
2025-04-11 13:00:51 -0500

    changes from dev

--- 40s-game/scripts/shop.gd
+++ 40s-game/scripts/shop.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+#@onready var characters = $Characters
+@onready var character: Node2D = $"Character sprites/character"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
