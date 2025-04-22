56e3be76402604bb07bee8a20482c7459e62440f
nigellat <115743236+nigellat@users.noreply.github.com>
2025-04-22 13:12:18 -0500

    updated variables!!
    -speechbubble working just needs texture to be resized!!

--- 40s-game/scenes/SpeechBubble.gd
+++ 40s-game/scenes/SpeechBubble.gd
@@ -8,12 +8,15 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	#print("portrait: ", portrait)
-	#print("label: ", label)
+	#access the global variables
+	var toppings = SignalBus.unlockedToppings
+	var drinks = SignalBus.unlockedDrinks
+	
 	#will have to change this variabel to be the randomized toppings that are picked!!??
-	var imageSelected = "res://sprites/kitchen/toppings - dispenser/brown sugar.PNG"
+	var toppingSelected = "res://sprites/kitchen/toppings - dispenser/brown sugar.PNG"
 	var message = "order text here!"
-	set_speech(imageSelected, message)
+	setBubble(toppingSelected, message)
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -21,12 +24,9 @@ func _process(delta: float) -> void:
 	pass
 
 
-func set_speech(image: String, order: String)-> void:
+func setBubble(image_path: String, order: String)-> void:
 	#portrait.texture = load(image_path)
 	#text_label.text = dialogue_text
-	var texture = load(image)
-	if texture:
-		image.texture = texture
-	#else:
-		#push_error("Couldn't load image: " + image_path)
+	var texture = load(image_path)
+	image.texture = texture
 	label.text = order
