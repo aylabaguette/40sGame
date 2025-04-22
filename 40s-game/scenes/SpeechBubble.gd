382ccbf24a792652b463ce0a537da35d6f148c72
nigellat <115743236+nigellat@users.noreply.github.com>
2025-04-22 12:58:44 -0500

    updated variable names and stuff!!

--- 40s-game/scenes/SpeechBubble.gd
+++ 40s-game/scenes/SpeechBubble.gd
@@ -2,18 +2,18 @@ extends Node2D
 
 #@onready var label: Label = $Panel/Label # Reference to the Label node
 #@onready var speechBubble: Node2D = $"."
-
-@onready var portrait: TextureRect = $Panel/Portrait
-@onready var text_label: Label = $Panel/Label
+@onready var image: TextureRect = $Panel/image
+@onready var label: Label = $Panel/Label
 @onready var panel: Panel = $Panel
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("portrait: ", portrait)
-	print("text_label: ", text_label)
-	var x = "res://sprites/kitchen/toppings - dispenser/brown sugar.PNG"
-	var message = "Uhwefq!"
-	set_speech(x, message)
+	#print("portrait: ", portrait)
+	#print("label: ", label)
+	#will have to change this variabel to be the randomized toppings that are picked!!??
+	var imageSelected = "res://sprites/kitchen/toppings - dispenser/brown sugar.PNG"
+	var message = "order text here!"
+	set_speech(imageSelected, message)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -21,12 +21,12 @@ func _process(delta: float) -> void:
 	pass
 
 
-func set_speech(image_path: String, dialogue_text: String)-> void:
+func set_speech(image: String, order: String)-> void:
 	#portrait.texture = load(image_path)
 	#text_label.text = dialogue_text
-	var tex = load(image_path)
-	if tex:
-		portrait.texture = tex
-	else:
-		push_error("Couldn't load image: " + image_path)
-	text_label.text = dialogue_text
+	var texture = load(image)
+	if texture:
+		image.texture = texture
+	#else:
+		#push_error("Couldn't load image: " + image_path)
+	label.text = order
