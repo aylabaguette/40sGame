619aa780f331cd449a5c1c7ebc6488db7c4463a1
aylabaguette <114880593+aylabaguette@users.noreply.github.com>
2025-04-22 21:51:54 -0500

    yaaaaay toppings work now

--- 40s-game/scenes/lids.gd
+++ 40s-game/scenes/lids.gd
@@ -11,10 +11,13 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if(holdingLid == true):
+		#make the single lid follow the mouse
 		lid_drag_.position = get_global_mouse_position()
 
+#called when the button which is on the stack of lids is pressed
 func _on_button_pressed() -> void:
 	holdingLid = true 
+	#makes the lid that follows the mouse visible 
 	lid_drag_.visible = true
 
 	
--- 40s-game/scenes/toppings.gd
+++ 40s-game/scenes/toppings.gd
@@ -1,4 +1,5 @@
 extends Node2D
+#initialize sprites for topping cubs
 @onready var lychee_jelly: Sprite2D = $"lychee jelly"
 @onready var coconut_jelly: Sprite2D = $"coconut jelly"
 @onready var mango_popping: Sprite2D = $"mango popping"
@@ -7,35 +8,52 @@ extends Node2D
 @onready var tapioca: Sprite2D = $tapioca
 @onready var popping_pearls: Sprite2D = $"popping pearls"
 
+const COCONUT_JELLY_CUP = preload("res://sprites/kitchen/toppings - dispenser/toppings drag/coconut_jelly_cup.png")
+const LYCHEE_JELLY_CUP = preload("res://sprites/kitchen/toppings - dispenser/toppings drag/lychee_jelly_cup.png")
+const MANGO_POPPING_CUP = preload("res://sprites/kitchen/toppings - dispenser/toppings drag/mango_popping_cup.png")
+const MILK_FOAM_CUP = preload("res://sprites/kitchen/toppings - dispenser/toppings drag/milk_foam_cup.png")
+const POPPING_PEARL_CUP = preload("res://sprites/kitchen/toppings - dispenser/toppings drag/popping_pearl_cup.png")
+const RED_BEAN_CUP = preload("res://sprites/kitchen/toppings - dispenser/toppings drag/red_bean_cup.png")
+const TAPIOCA_CUP = preload("res://sprites/kitchen/toppings - dispenser/toppings drag/tapioca_cup.png")
+
+#sprite for the topping that follows mouse
 @onready var topping_drag: Sprite2D = $"../toppingDrag"
 
+var toppingFollowMouse = false
+
+#arrays for different sprite arrays
 var unlockedToppings
 
 var totalToppings = []
 
-var unlockedToppingsCup = []
+var unlockedToppingsCup = [LYCHEE_JELLY_CUP,COCONUT_JELLY_CUP,MANGO_POPPING_CUP,MILK_FOAM_CUP,RED_BEAN_CUP,TAPIOCA_CUP,POPPING_PEARL_CUP]
 var totalToppingsCup = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	
+	#initialize topping array w sprites
+	#very important for this to be here don't move it!
 	unlockedToppings = [lychee_jelly,coconut_jelly,mango_popping,milk_foam,red_bean,tapioca,popping_pearls]
-	
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
-
+	if(toppingFollowMouse):
+		topping_drag.position = get_global_mouse_position()
+	
 func _input(event: InputEvent) -> void:
-	for topping in range(unlockedToppings.size()):
-		var currentTopping
+	#for loop that loops through array of toppings
+	if(Input.is_action_just_pressed("mouse click")):
+		for topping in range(unlockedToppings.size()):
+			var currentTopping
+			currentTopping = unlockedToppings[topping]
+			var mousePos = get_global_mouse_position()
+			#gets mouse position local to specific sprite looking at
+			var local_mouse_pos = currentTopping.to_local(mousePos)
 		
-		currentTopping = unlockedToppings[topping]
-		var mousePos = get_global_mouse_position()
-		var local_mouse_pos = currentTopping.to_local(mousePos)
-									
-		if(currentTopping.get_rect().has_point(local_mouse_pos)):
-			print(currentTopping)
-				
+			#basically saying if the mouse is within the rect 
+			if(currentTopping.get_rect().has_point(local_mouse_pos)):
+				topping_drag.texture = unlockedToppingsCup[topping]
+				toppingFollowMouse = true 
 			
 			
