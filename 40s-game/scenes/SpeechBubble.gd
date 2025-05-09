1b24a64e7800fe742f10dc1d4e40e169856a0cda
nigellat <115743236+nigellat@users.noreply.github.com>
2025-05-09 15:09:47 -0500

    worked on speech bubble layout !!

--- 40s-game/scenes/SpeechBubble.gd
+++ 40s-game/scenes/SpeechBubble.gd
@@ -3,6 +3,10 @@ extends Node2D
 #@onready var label: Label = $Panel/Label # Reference to the Label node
 #@onready var speechBubble: Node2D = $"."
 @onready var image: TextureRect = $Panel/image
+
+@onready var image2: TextureRect = $Panel/image2
+@onready var image3: TextureRect = $Panel/image3
+
 @onready var label: Label = $Panel/Label
 @onready var label2: Label = $Panel/Label2
 @onready var label3: Label = $Panel/Label3
@@ -18,13 +22,16 @@ func _ready() -> void:
 	
 	#will have to change this variabel to be the randomized toppings that are picked!!??
 	var toppingSelected = "res://sprites/kitchen/toppings - dispenser/brown sugar.PNG"
+	var toppingSelected2 = "res://sprites/kitchen/toppings - dispenser/coffee.PNG"
+	var flavourSelected = "res://sprites/kitchen/toppings - dispenser/lemon.PNG"
+
 	#var message = "order text here!"
 	#I woudl like to order _flavour_ (photo) with _topping1_ (photo) and _topping2_ (photo) pelase!
 	var message1 = "I would like to order __ with __ and __ please!"
 	var message2 = " with "
 	var message3 = " and "
 	var message4 = " please!"
-	setBubble(toppingSelected, message1, message2, message3, message4)
+	setBubble(toppingSelected, toppingSelected2, flavourSelected, message1, message2, message3, message4)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -32,9 +39,13 @@ func _process(delta: float) -> void:
 	pass
 
 
-func setBubble(image_path: String, order1: String, order2: String, order3: String, order4: String)-> void:
+func setBubble(image_path: String, image_path2: String, image_path3: String, order1: String, order2: String, order3: String, order4: String)-> void:
 	var texture = load(image_path)
+	var texture2 = load(image_path2)
+	var texture3 = load(image_path3)
 	image.texture = texture
+	image.texture = texture2
+	image.texture = texture3
 	label.text = order1
 	label2.text = order2
 	label3.text = order3
